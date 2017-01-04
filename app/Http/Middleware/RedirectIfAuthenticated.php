fb222273cd1e8ff3599203a8ba93496034de78a3
at-phuctv <phuctv@asiantech.com>
2017-01-04 09:23:00 +0700

    create new project

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request [description]
+     * @param \Closure                 $next    [description]
+     * @param string|null              $guard   [description]
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
