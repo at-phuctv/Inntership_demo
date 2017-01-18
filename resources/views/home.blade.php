e07f52d0a7e06436ab81947690174a219eefd815
at-phuctv <phuctv@asiantech.com>
2017-01-18 08:22:42 +0700

    Chang file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,9 +18,7 @@
                     <p>{{ $value->acreage}}</p>
                     <p>{{ $value->price}}</p>
                     <p>{{ $value->address}}</p>
-                    <button class="follow_post_user" value="{{$value->id}}" dataUser="
-                    {{ (!(Auth::guest()))? Auth::user()->email :''}}
-                    "><i class="glyphicon glyphicon-eye-open"></i></button>
+                    <button class="follow_post_user" value="{{$value->id}}" dataUser="{{ (!(Auth::guest()))? Auth::user()->email :''}}"><i class="glyphicon glyphicon-eye-open"></i></button>
                   </div>
                    <div class="col-md-4 welcome-img">
                      <img class="img-responsive " src="{{ trans('constants.image')}}" alt="">  
